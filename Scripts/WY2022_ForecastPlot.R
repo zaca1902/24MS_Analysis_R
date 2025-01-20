0ca58dd58342d093f592aa1c12d3dd7b9dc3b06d
Zachary Carpenter <zaca1902@colorado.edu>
2025-01-20 16:29:47 -0700

    Restructured code to change display order of plot components

--- Scripts/WY2022_ForecastPlot.R
+++ Scripts/WY2022_ForecastPlot.R
@@ -14,24 +14,34 @@ WY2022_ForecastPlot <- function()
   min = data$Min[1:n]
   max = data$Max[1:n]
  
-  # Create Initial Plot - with Most Probable Forecast
+  # Create Initial Plot Area
   xlim = 24
   ylim = 20
   
-  plot(x, most, type = "l", xlim = rev(range(x)), ylim = c(0, ylim), 
-       col = "forestgreen", lwd = 2, lty = 2, xlab = "", ylab = "", xaxt = "n",
-       yaxt = "n")
+  plot(x, most, type = "n", xlim = rev(range(x)), ylim = c(0, ylim), xlab = "", 
+       ylab = "", xaxt = "n", yaxt = "n")
   title(main = "Water Year 2022 - Lake Powell", 
         xlab = "Months Away from EOWY 2022", 
         ylab = "Unregulated Inflow Volume (MAF)")
   
-  # Add Min/Max Prob Forecast points
-  points(x[1:n], min, col = "red", pch = "-", cex = 2.5)
-  points(x[1:n], max, col = "blue", pch = "-", cex = 2.5)
+  # Add Grid Lines - grid command malfunctioning
+  # grid(nx = length(seq(24, 0, by = -2)), ny = length(seq(0, 20, by = 2)), 
+  #      col = "gray")
+  for (i in 1:xlim)
+  {if(i %% 2 == 0){abline(v = i, col = "grey", lty = 3)}}
+  for (i in 1:ylim)
+  {if(i %% 2 == 0){abline(h = i, col = "grey", lty = 3)}}
   
   # Add actual accumulated unregulated inflow volume (maf)
   auiv = 6.084
   abline(h = auiv, col = "black", lwd = 2)
+  
+  # Add Most Prob Forecast line
+  lines(x, most, lwd = 2, lty = 2, col = "forestgreen")
+  
+  # Add Min/Max Prob Forecast points
+  points(x[1:n], min, col = "red", pch = "-", cex = 2.5)
+  points(x[1:n], max, col = "blue", pch = "-", cex = 2.5)
   
   # Add Legend
   legend("topright", legend = c("Maximum Probable", "Most Probable", 
@@ -46,13 +56,6 @@ WY2022_ForecastPlot <- function()
   axis(1, at = seq(xlim, 0, by = -1), labels = seq(xlim, 0, by = -1))
   axis(2, at = seq(0, ylim, by = 2), labels = seq(0, ylim, by = 2))
   
-  # Add Grid Lines - grid command malfunctioning
-  # grid(nx = length(seq(24, 0, by = -2)), ny = length(seq(0, 20, by = 2)), 
-  #      col = "gray")
-  for (i in 1:xlim)
-  {if(i %% 2 == 0){abline(v = i, col = "grey", lty = 3)}}
-  for (i in 1:ylim)
-  {if(i %% 2 == 0){abline(h = i, col = "grey", lty = 3)}}
-  
+
   # 
 }
