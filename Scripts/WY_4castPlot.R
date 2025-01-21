1d3c8bf3f25b7758bc2215bf5b49ffd9ad75d4a0
Zachary Carpenter <zaca1902@colorado.edu>
2025-01-21 15:50:52 -0700

    Edit plot label text strings and add auiv line

--- Scripts/WY_4castPlot.R
+++ Scripts/WY_4castPlot.R
@@ -18,7 +18,30 @@ WY_4castPlot <- function(data, yr, resname, auiv)
   # Created by: Zachary Carpenter
   # Created on: 1/21/2025
   
+  n = nrow(data)-3    # Creates length for Min/Max to prevent data overlap
   
+  x = data$MonthsAway
+  most = data$Most
+  min = data$Min[1:n]
+  max = data$Max[1:n]
   
+  # Create Initial Plot Area
+  xlim = 24
+  ylim = 20
   
+  plot(x, most, type = "n", xlim = rev(c(0,xlim)), ylim = c(0, ylim), xlab = "", 
+       ylab = "", xaxt = "n", yaxt = "n", yaxs = "i")
+  title(main = paste("Water Year ", yr, "- Lake ", resname), 
+        xlab = "", ylab = "Unregulated Inflow Volume (MAF)")
+  
+  # Add Grid Lines - grid command malfunctioning
+  # grid(nx = length(seq(24, 0, by = -2)), ny = length(seq(0, 20, by = 2)), 
+  #      col = "gray")
+  for (i in 0:xlim)
+  {if(i %% 2 == 0){abline(v = i, col = "grey", lty = 3)}}
+  for (i in 1:ylim)
+  {if(i %% 2 == 0){abline(h = i, col = "grey", lty = 3)}}
+  
+  # Add actual accumulated unregulated inflow volume (maf)
+  abline(h = auiv, col = "black", lwd = 2)
 }
