2f9c5ad626beba12ad9e481b983fbdc6de344dee
Zachary Carpenter <zaca1902@colorado.edu>
2025-01-21 15:31:36 -0700

    Add documentation to new script for creating plots

---
+++ Scripts/WY_4castPlot.R
@@ -0,0 +1,24 @@
+WY_4castPlot <- function(data, yr, resname, auiv)
+{
+  # The following code creates a plot of "hind casts" from the 24 Month Study
+  # for the accumulated unregulated inflow volume (AUIV) at the end of a water 
+  # year for a given reservoir.The format for these plots is derived from
+  # Figure 5 of White Paper #7 from the CCRS 
+  # (https://qcnr.usu.edu/coloradoriver/files/news/White-Paper-7.pdf).
+  
+  # Inputs:
+  #   data: the file path/data file that contains the 24 months of 24-MS data
+  #         for projecting the AUIV at the end of the water year of interest.
+  #   yr: the year of interest for the projections
+  #   resname: the reservoir name of interest
+  #   auiv: the actual value for the AUIV at the end of the WY of interest
+  
+  # Outputs: Plot of the "hind casts"
+  
+  # Created by: Zachary Carpenter
+  # Created on: 1/21/2025
+  
+  
+  
+  
+}
