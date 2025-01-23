30d62ad329c740a313b8cf66efb6159fa1eaae66
Zachary Carpenter <zaca1902@colorado.edu>
2025-01-22 17:23:46 -0700

    added indices for data read to remove totals, added commented outline for code structure

--- Data/histrec_plots.R
+++ Data/histrec_plots.R
@@ -5,13 +5,22 @@ histrecplots <- function(newrec_file, oldrec_file)
   old = read.csv(oldrec_file, header = TRUE, skip = 2)
   
   # Parse data set
-  new_med = new$Med
-  new_25 = new$Min25
-  new_75 = new$Max75
+  month = new$month
+  new_med = new$Med[2:13]
+  new_25 = new$Min25[2:13]
+  new_75 = new$Max75[2:13]
   
-  old_med = old$Med
-  old_25 = old$Min25
-  old_75 = old$Max75
+  old_med = old$Med[2:13]
+  old_25 = old$Min25[2:13]
+  old_75 = old$Max75[2:13]
   
+  # Old Record Bar Chart
   
+  # New Record Bar Chart
+  
+  # Median Comparison Bar Chart
+  
+  # 25th %-ile Comparison Bar Chart
+  
+  # 75th %-ile Comparison Bar Chart
 }
