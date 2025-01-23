ff294848b507d3b90bdab5837cc527570af2a5e1
Zachary Carpenter <zaca1902@colorado.edu>
2025-01-22 17:17:33 -0700

    add data parse commands

--- Data/histrec_plots.R
+++ Data/histrec_plots.R
@@ -4,5 +4,14 @@ histrecplots <- function(newrec_file, oldrec_file)
   new = read.csv(newrec_file, header = TRUE, skip = 2)
   old = read.csv(oldrec_file, header = TRUE, skip = 2)
   
-  # Parse dataset
+  # Parse data set
+  new_med = new$Med
+  new_25 = new$Min25
+  new_75 = new$Max75
+  
+  old_med = old$Med
+  old_25 = old$Min25
+  old_75 = old$Max75
+  
+  
 }
